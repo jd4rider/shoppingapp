571e6d5685ad64fe7f6e1050c6b90f1ed5204576
Jonathan D. Forrider <jd4rider@gmail.com>
2018-06-27 00:45:53 -0600

    Angular Baby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title = 'Shopping';
+  results = {};
+  products = [];
+
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit() : void {
+    interface Product {
+      name: string;
+      category: string;
+      price: number;
+    }
+
+    this.http.get<Product>('http://localhost:51276/api/Products').subscribe(data => {
+      this.results = data;
+    });
+  }
 }
